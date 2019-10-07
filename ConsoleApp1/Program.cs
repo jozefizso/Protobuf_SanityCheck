13c4762aeaa76197384b4abf5b60906034ede46b
Jozef Izso <jozef.izso@gmail.com>
2019-10-07 22:17:08 +0200

    grpc 1.23.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var c = new Channel("127.0.0.1", ChannelCredentials.Insecure);
+            var s = new HelloWorldService.HelloWorldServiceClient(c);
         }
     }
 }
